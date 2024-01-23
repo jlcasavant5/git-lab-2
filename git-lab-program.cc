b6f8b8380a46ce7d4f41ae8a326e542ffe1703be
JLCasavant5 <JC552321@ohio.edu>
2024-01-23 12:12:33 -0500

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jules Casavant
+ *        Date: January 23, 2024
+ * Description: 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << end1;
+    return 0;
+}// main
+
